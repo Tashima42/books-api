b8f39a669b7fd16864f126d7a242c4ffaa14a971
Pedro Tashima <pedrotashima42@gmail.com>
2020-10-18 01:17:59 -0300

    feat: added lumen base and migrations

---
+++ books/database/migrations/2020_10_18_040810_create_books_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->increments('book_id');
+            $table->string('name');
+            $table->string('edition');
+            $table->string('isbn');
+            $table->string('published');
+            $table->string('language');
+            $table->string('description');
+            $table->integer('link_id');
+            $table->integer('author_id');
+            $table->integer('publisher_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
